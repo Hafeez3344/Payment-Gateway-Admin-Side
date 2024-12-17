08fcb931a22ea1a8e1035655d7daed582d8ce861
IRFAN UL HAQ <khan3344kpk@gmail.com>
2024-12-17 18:47:44 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const BACKEND_URL = "http://192.168.1.4:8888";
+const BACKEND_URL = "http://192.168.1.21:8888";
 
 export const fn_loginAdminApi = async (data) => {
     try {
